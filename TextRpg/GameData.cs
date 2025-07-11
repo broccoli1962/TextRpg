5d28ac4959f0078b6e71a0d91fa7c9fac29695fd
broccoli1962 <aq0011@naver.com>
2025-07-11 12:34:08 +0900

    add new item
    add new item

--- TextRpg/GameData.cs
+++ TextRpg/GameData.cs
@@ -40,6 +40,8 @@ namespace TextRpg
                 new("낡은 검", "쉽게 볼 수 있는 낡은 검 입니다.", ItemType.Weapon, 2, 0, 600),
                 new("청동 도끼", "어디선가 사용됐던거 같은 도끼입니다.", ItemType.Weapon, 5, 0, 1500),
                 new("스파르타의 창", "스파르타의 전사들이 사용했다는 전설의 창입니다.", ItemType.Weapon, 7, 0, 2500),
+                new("기름 먹은 활", "미끈거리는 감촉이 느껴지는 활이다.", ItemType.Weapon, 10, 0, 2000),
+                new("삭아버린 천 갑옷", "이제는 갑옷이라 불릴 수도 없을 듯하다.", ItemType.Armor, 0, 1, 200)
             };
         }
 
